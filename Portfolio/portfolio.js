352eef2181d4d9b58f1cb2e23b225c5ba09cb4f8
SamCrosby <SamanthaCrosby97@Outlook.com>
2018-04-30 17:48:43 +0100

    Add files via upload
    Fresh upload

--- Portfolio/portfolio.js
+++ Portfolio/portfolio.js
@@ -1,34 +1,18 @@
 
-
+//This is the javascript for the tabs on the index page
 function openSkill(evt, skillName) {
     var i, skill, tablinks;
+    //It retrieves the elements with the skill class and hides them
     skill = document.getElementsByClassName("skill");
     for (i = 0; i < skill.length; i++) {
         skill[i].style.display = "none";
     }
+    //It retrieves the elements with the tablinks class and removes the active class
     tablinks = document.getElementsByClassName("tablinks");
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
+    //It shows the current tab and sets sets the current tab that is clicked to active 
     document.getElementById(skillName).style.display = "block";
     evt.currentTarget.className += " active";
 }
-
-
-  // When the user scrolls the page, execute myFunction
-  // window.onscroll = function() {myFunction()};
-  //
-  // // Get the header
-  // var header = document.getElementById("header");
-  //
-  // // Get the offset position of the navbar
-  // var sticky = header.offsetTop;
-  //
-  // // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-  // function myFunction() {
-  //   if (window.pageYOffset >= sticky) {
-  //     header.classList.add("sticky");
-  //   } else {
-  //     header.classList.remove("sticky");
-  //   }
-  // }
